3d6230434b80ceccb93b51f2d6b756bab0e0807d
Radu Florin MARINOIU <rmarinoiu@bitdefender.com>
2019-03-25 12:44:37 +0200

    Fixed unique names in phonebook and added proper responses

--- phonebook/phonebook.py
+++ phonebook/phonebook.py
@@ -8,6 +8,11 @@ REQUEST_TYPE = "type"
 NAME_FIELD = "_name"
 PHONE_FIELD = "phone"
 COUNTRY_FIELD = "country"
+RESPONSE_STATUS_CODE = "status_code"
+RESPONSE_DATA = "data"
+STATUS_SUCCESS = 200
+STATUS_NOT_FOUND = 404
+STATUS_BAD_REQUEST = 400
 
 PHONEBOOK_ENTRY = {
     NAME_FIELD: "",
@@ -27,24 +32,40 @@ def UPDATE_DATABASE():
     with open(DATABASE_PATH, "wb") as f:
         json.dump(DATABASE, f, indent=4, sort_keys=True)
 
+
+
 def POST_REQUEST(request_data):
     pb_entry = deepcopy(PHONEBOOK_ENTRY)
+    for pb_entry in DATABASE:
+        if request_data[NAME_FIELD] == pb_entry[NAME_FIELD]:
+            return {
+                RESPONSE_STATUS_CODE: STATUS_BAD_REQUEST
+            }
     for key in PHONEBOOK_ENTRY:
         if key in request_data:
             pb_entry[key] = request_data[key]
         else:
-            return "ERROR" #TODO: as json
+            return {
+                RESPONSE_STATUS_CODE: STATUS_BAD_REQUEST
+            }
     DATABASE.append(pb_entry)
     UPDATE_DATABASE()
-    return "OK"
+    return {
+        RESPONSE_STATUS_CODE: STATUS_SUCCESS,
+        RESPONSE_DATA: pb_entry
+    }
     
 def GET_REQUEST(request_data):
     if NAME_FIELD in request_data:
-        name_value = request_data[NAME_FIELD]
-        for entry in DATABASE:
-            if name_value == entry[NAME_FIELD]:
-                return entry
-    return "ERROR"
+        for pb_entry in DATABASE:
+            if request_data[NAME_FIELD] == pb_entry[NAME_FIELD]:
+                return {
+                    RESPONSE_STATUS_CODE: STATUS_SUCCESS,
+                    RESPONSE_DATA: pb_entry
+                }
+    return {
+        RESPONSE_STATUS_CODE: STATUS_NOT_FOUND
+    }
     
 def UPDATE_REQUEST(request_data):
     if NAME_FIELD in request_data:
@@ -55,18 +76,30 @@ def UPDATE_REQUEST(request_data):
                     if key in request_data:
                         pb_entry[key] = request_data[key]
                 UPDATE_DATABASE()
-                return pb_entry
-    return "ERROR"
+                return {
+                    RESPONSE_STATUS_CODE: STATUS_SUCCESS,
+                    RESPONSE_DATA: pb_entry
+                }
+    return {
+        RESPONSE_STATUS_CODE: STATUS_NOT_FOUND
+    }
     
 def DELETE_REQUEST(request_data):
     if NAME_FIELD in request_data:
         name_value = request_data[NAME_FIELD]
-        for entry in DATABASE:
-            if name_value == entry[NAME_FIELD]:
-                DATABASE.remove(entry)
+        for pb_entry in DATABASE:
+            if name_value == pb_entry[NAME_FIELD]:
+                DATABASE.remove(pb_entry)
                 UPDATE_DATABASE()
-                return "OK"
-    return "ERROR"
+                return {
+                    RESPONSE_STATUS_CODE: STATUS_SUCCESS,
+                    RESPONSE_DATA: pb_entry
+                }
+    return {
+        RESPONSE_STATUS_CODE: STATUS_NOT_FOUND
+    }
+
+
 
 REQUEST_TYPES = {
     "POST": POST_REQUEST,
@@ -77,14 +110,12 @@ REQUEST_TYPES = {
 
 
 
-
-
 def main():
     if os.path.exists(DATABASE_PATH):
         READ_DATABASE()
     request = json.load(sys.stdin)
     if REQUEST_TYPE in request and REQUEST_DATA in request and request[REQUEST_TYPE] in REQUEST_TYPES and request:
-        print REQUEST_TYPES[ request[REQUEST_TYPE] ]( request[REQUEST_DATA] )
+        print json.dumps(REQUEST_TYPES[ request[REQUEST_TYPE] ]( request[REQUEST_DATA] ), sort_keys=True)
 
 if __name__ == "__main__":
     main()
